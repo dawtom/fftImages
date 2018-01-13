daf5e8e00549ed1d1d1e0f584e6f933325f1c698
Dawid <datom2912@interia.pl>
2018-01-13 16:35:13 +0100

    also file

--- zad1.py
+++ zad1.py
@@ -5,21 +5,29 @@ import matplotlib.pyplot as plt
 
 def main():
     textExamplePath = 'textExample.png'
-    ePath = 'eExample.png'
+    ePath = 'foooo.png'
 
     source = 255 - ndimage.imread(textExamplePath, flatten=True)
 
     pattern = 255 - ndimage.imread(ePath, flatten=True)
 
+
+
+    # plt.imshow(pattern, cmap='jet')
+    plt.imshow(source, cmap='jet')
+    # plt.savefig('foooo.png')
+    plt.show()
+
+
     fi = np.fft.fft2(source)
     fp = np.fft.fft2(np.rot90(pattern, 2), fi.shape)
     m = np.multiply(fi, fp)
-
+    #
     corr = np.fft.ifft2(m)
     corr = np.abs(corr)
     corr = corr.astype(float)
-
-    corr[corr < 0.9 * np.amax(corr)] = 0
+    #
+    corr[corr < 0.8 * np.amax(corr)] = 0
 
     suma = ndimage.label(corr)
     print(suma[1])
@@ -28,7 +36,7 @@ def main():
         for j in range(len(corr[0])):
             if corr[i][j] != 0:
                 print(str(corr[i][j]) + ' ' + str(i) + ' ' + str(j), end=' ')
-    #     print('')
+        print('')
 
     plt.imshow(corr, cmap='jet')
     plt.savefig('foo.png')
